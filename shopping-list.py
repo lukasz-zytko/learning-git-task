06d554703d3a38441f14e18dc20569874d9d0f14
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-11-19 22:00:29 +0100

    Add printing list

--- shopping-list.py
+++ shopping-list.py
@@ -3,9 +3,5 @@ lista_zakupow = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 
-x=0
 for sklep, produkt in lista_zakupow.items():
     print(f"Odiwedzam sklep {sklep} i kupuję tam {produkt}")
-    x = x + len(produkt)
-
-print(f"W sumie kupuję {x} produktów")
