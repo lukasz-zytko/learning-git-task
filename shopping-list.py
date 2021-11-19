41c864a71e3ddc25a2246dc2f8ed6b5dc59f65f4
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-11-19 22:10:22 +0100

    Add counter of products

--- shopping-list.py
+++ shopping-list.py
@@ -9,6 +9,9 @@ for sklep, produkt in lista_zakupow.items():
     for i in produkt:
         new_list[sklep.capitalize()].append(i.title())
 
-
+x = 0
 for sklep, produkt in new_list.items():
     print(f"Odiwedzam sklep {sklep} i kupuję tam {produkt}")
+    x = x + len(produkt)
+
+print(f"W sumie kupuję {x} produktów")
